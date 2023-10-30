4b1dd34f6f5f83f7795ba02c07310f7836dd5134
Patryk Ostern <ostern.patryk02@gmail.com>
2023-10-30 11:31:17 +0100

    add initial grid

--- frontend/src/components/DropVideoSection.js
+++ frontend/src/components/DropVideoSection.js
@@ -4,6 +4,12 @@ export function DropVideoSection() {
     return (
         <div class="VideoSection">
             <h2>Upload Video</h2>
+            <div class="GridWrapper">
+                <div class="VideoUpload">
+                </div>
+                <div class="VideoDisplay">
+                </div>
+            </div>
         </div>
     )
 }
