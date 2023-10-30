cae5c7432c8520dae813e26eb8460cb61c3a8ce3
Patryk Ostern <ostern.patryk02@gmail.com>
2023-10-30 12:40:57 +0100

    add section for upload and dispaly video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Header } from './components/Header';
 
 function App() {
   return (
-    <div class="App">
+    <div className="App">
       <Header></Header>
       <DropVideoSection></DropVideoSection>
     </div>
--- frontend/src/components/DropVideoSection.js
+++ frontend/src/components/DropVideoSection.js
@@ -1,13 +1,36 @@
 import './DropVideoSection.css';
+import React, { useState } from 'react';
+import { Upload, Button } from "antd";
 
 export function DropVideoSection() {
+    const [videoSrc , seVideoSrc] = useState("");
+
+    const handleChange = ({file}) => {
+        var url = URL.createObjectURL(file.originFileObj);
+        seVideoSrc(url);
+    };
+
     return (
-        <div class="VideoSection">
-            <h2>Upload Video</h2>
-            <div class="GridWrapper">
-                <div class="VideoUpload">
+        <div className="VideoSection">
+            <div className="UploadButton">
+                <Upload className="mt-3 mb-3"
+                    accept=".mp4"
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture"
+                    maxCount={1}
+                    onChange={handleChange}
+                    >
+                    <Button>
+                    Upload Video
+                    </Button>
+                </Upload>
+            </div>
+
+            <div className="GridWrapper">
+                <div className="VideoDisplay">
+                    <video src={videoSrc} controls></video>
                 </div>
-                <div class="VideoDisplay">
+                <div className='VideoUpload'>
                 </div>
             </div>
         </div>
